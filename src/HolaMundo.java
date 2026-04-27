701adfaeed984e7b5b67cde7d910fcc26278ec5a
luisgc020300-beep <luisgc020300@gmail.com>
2026-04-27 18:11:30 +0200

    bccbjksh

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola planeta");
         System.out.println("Como te llamas");
     }
 }
