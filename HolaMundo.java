8eca8448934c476ec9775ef7a00be73676ebe965
luisgc020300-beep <luisgc020300@gmail.com>
2026-04-28 16:23:02 +0200

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,6 @@ public class HolaMundo {
     static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Como te llamas");
+        System.out.println("ya casi esta la practica"
     }
 }
